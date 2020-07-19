1cb1339299543960b3cd7dbe1bf8200be45d3fea
sai pramod <saipramod47@gmail.com>
2020-07-19 20:25:01 +0530

    Passport_facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/confusion'
+    'mongoUrl': 'mongodb://localhost:27017/confusion',
+    'facebook': {
+        clientId: '2680791405474565',
+        clientSecret: 'ada8166ca694b3e5006be9c1d6f972f9'
+    }
 }
